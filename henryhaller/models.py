59dc137b5c28fb86c34e9fcf37e594d8e35fa521
Henry Haller <henrybhaller@gmail.com>
2013-08-08 20:46:06 +0000

    blog progress

--- henryhaller/models.py
+++ henryhaller/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import django.utils.timezone
 
 # Create your models here.
 
@@ -8,4 +9,10 @@ class BlogPost(models.Model):
 	text = models.TextField()
 	def __unicode__(self): return self.title
 	def pretty_age(self):
-		pass
+		age_diff = django.utils.timezone.now() - self.pub_date
+		days, seconds = age_diff.days, age_diff.seconds
+		if days == 0 and seconds < 60: return "less than 1 minute old"
+		if days == 0 and seconds < 3600: return "less than1 hour old"
+		if days == 0 and seconds < (24 * 60 * 60): return "less than 1 day old"
+		if days == 1: return "1 day old"
+		return str(days) + " days old"
